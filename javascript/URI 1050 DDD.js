590e06f0a612619efca1fe6fc56ef89c15f86a0e
danisacramento <danielleacsacramento@gmail.com>
2019-11-26 15:38:13 -0300

    exercício de hoje

---
+++ javascript/URI 1050 DDD.js
@@ -0,0 +1,8 @@
+var ddd = prompt("Por favor, digite o DDD do telefone");
+if(ddd ===61){
+    document.getElementsById("uf").innerHTML="Salvador";
+
+}
+else{
+    
+}
---
+++ javascript/ddd.js
@@ -0,0 +1,4 @@
+var ddd = prompt("Por favor, digite o DDD do telefone");
+switch(ddd){
+    
+}
