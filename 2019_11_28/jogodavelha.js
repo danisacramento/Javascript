b6c4434439df84d2adc382090b8a8988531ed1b3
wolfzerax <italoandrew@gmail.com>
2019-11-28 15:46:05 -0300

    exercício de hoje

---
+++ 2019_11_28/jogodavelha.js
@@ -0,0 +1,23 @@
+// jogador 1 = X
+// jogador 2 = O
+
+var vezDoJogador = 1;
+
+function joga(idDiv){
+    var textoInterno = document.getElementById(idDiv).innerHTML;
+    if (textoInterno == ""){
+        if (vezDoJogador == 1){
+            document.getElementById(idDiv).innerHTML="<0>";
+            vezDoJogador = 2;
+        }
+        else{
+            document.getElementById(idDiv).innerHTML="<x>";
+            vezDoJogador = 1;
+        }
+    }
+    mostraVez();
+}
+
+function mostraVez(){
+    document.getElementById("rodape").innerHTML = "<h1> Vez do Jogador "+vezDoJogador+"</h1>";
+}
